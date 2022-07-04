a162e2aa1cb0dda063db1a23d46cd8fe2b4576aa
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-07-04 20:31:05 -0300

    👓 Adicionado Toastify

---
+++ src/utils/toastify.js
@@ -0,0 +1,51 @@
+import { toast } from "react-toastify";
+
+function successMessage(message = "Mensagem") {
+  toast.success(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+}
+
+function errorMessage(message = "Mensagem") {
+  toast.error(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+}
+
+function warningMessage(message = "Mensagem") {
+  toast.warning(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+}
+
+function infoMessage(message = "Mensagem") {
+  toast.info(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+}
+
+export { successMessage, errorMessage, warningMessage, infoMessage };
